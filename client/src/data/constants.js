98677a849ad59d29e1f7a27cb2bbb5a5f7e5b7bf
Nimantha Supun <supunnimantha199@gmail.com>
2025-06-10 06:52:01 +0530

    feat(data): add constants for dropdown options including degree programs, years of study, departments, and research areas

---
+++ client/src/data/constants.js
@@ -0,0 +1,57 @@
+// Dropdown options for forms
+export const degreePrograms = [
+  'BSc IT',
+  'BSc Management',
+  'BSc Engineering',
+  'BA Business',
+  'BSc Computer Science',
+  'MBA'
+];
+
+export const yearsOfStudy = [
+  '1st Year',
+  '2nd Year',
+  '3rd Year',
+  '4th Year'
+];
+
+export const departments = [
+  'Computer Science',
+  'Management',
+  'Engineering',
+  'Business',
+  'Mathematics',
+  'Physics'
+];
+
+export const lecturerDesignations = [
+  'Lecturer',
+  'Senior Lecturer',
+  'Professor',
+  'Assistant Professor'
+];
+
+export const coordinatorDesignations = [
+  'Timetable Officer',
+  'Hall Manager',
+  'Academic Coordinator',
+  'Program Coordinator'
+];
+
+export const coordinatorDepartments = [
+  'Administration',
+  'Academic Affairs',
+  'Student Services',
+  'IT Services'
+];
+
+export const researchAreas = [
+  'Artificial Intelligence',
+  'Machine Learning',
+  'Data Science',
+  'Web Development',
+  'Mobile Development',
+  'Cybersecurity',
+  'Software Engineering',
+  'Database Systems'
+];
