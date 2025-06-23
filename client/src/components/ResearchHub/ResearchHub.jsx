6cacc99a5fa55d1ae3776d5283711ee055eec2cf
Nimantha Supun <supunnimantha199@gmail.com>
2025-06-23 12:08:06 +0530

    feat(ResearchHub): add ResearchHub component with routing for LandingPage, StudentDashboard, SupervisorList, and CollaborationFeed

---
+++ client/src/components/ResearchHub/ResearchHub.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import Layout from "./Layout/Layout.jsx";
+import LandingPage from "./Home/LandingPage.jsx";
+import StudentDashboard from "./Dashbord/StudentDashboard.jsx";
+import SupervisorList from "./Superviors/SupervisorList.jsx";
+import CollaborationFeed from './Collaboration/CollaborationFeed.jsx';
+
+const ResearchHub = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<LandingPage />} />
+          <Route path="/indashboard" element={<StudentDashboard />} />
+          <Route path="/supervisors" element={<SupervisorList />} />
+          <Route path="/collaboration" element={<CollaborationFeed />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default ResearchHub;
